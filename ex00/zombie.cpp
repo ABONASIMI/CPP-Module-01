0881ad2b065518be160b047b82d25b24fed7eaab
ABONASIMI <abonasimi127@gmail.com>
2026-09-23 23:13:05 +0200

    added

---
+++ ex00/zombie.cpp
@@ -0,0 +1,18 @@
+#include "zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
+}
+
+
+void Zombie::announce()
+{
+    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+
+Zombie::~Zombie()
+{
+    std::cout << this->name << " is destroyed" << std::endl;
+}
