fe7084ab60c2b0b51af463abb9468b4d449480da
A-Aboo <a.abozer.n@gmail.com>
2026-09-25 23:45:44 +0200

    add ./ex03/HumanB.cpp

---
+++ ex03/HumanB.cpp
@@ -0,0 +1,24 @@
+#include "HumanB.hpp"
+#include <iostream>
+
+HumanB::HumanB(std::string name)
+{
+    this->name = name;
+    this->weapon = NULL;
+}
+
+void HumanB::setWeapon(Weapon& weapon)
+{
+    this->weapon = &weapon;
+}
+
+void HumanB::attack()
+{
+    if (this->weapon != NULL)
+    {
+        std::cout << this->name
+                  << " attacks with their "
+                  << this->weapon->getType()
+                  << std::endl;
+    }
+}
